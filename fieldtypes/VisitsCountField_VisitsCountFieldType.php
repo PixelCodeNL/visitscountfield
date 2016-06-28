933dc680faed6465821b16a9b251e3014afa68bc
Jordy Versmissen <jordy@pixelcode.nl>
2016-06-28 15:34:52 +0200

    Fixed javascript error in backend Fixed fatal error when value is string instead of a model instance

--- fieldtypes/VisitsCountField_VisitsCountFieldType.php
+++ fieldtypes/VisitsCountField_VisitsCountFieldType.php
@@ -29,20 +29,12 @@ class VisitsCountField_VisitsCountFieldType extends BaseFieldType
     {
         if (!$value) {
             $value = new VisitsCountField_VisitsCountModel();
+        } elseif (is_string($value)) {
+            $value = new VisitsCountField_VisitsCountModel(['visitsCount' => $value]);
         }
 
         $id = craft()->templates->formatInputId($name);
         $namespacedId = craft()->templates->namespaceInputId($id);
-
-        $jsonVars = array(
-            'id' => $id,
-            'name' => $name,
-            'namespace' => $namespacedId,
-            'prefix' => craft()->templates->namespaceInputId(""),
-        );
-
-        $jsonVars = json_encode($jsonVars);
-        craft()->templates->includeJs("$('#{$namespacedId}').VisitsCountField_VisitsCountFieldType(" . $jsonVars . ");");
 
         $variables = array(
             'id' => $id,
@@ -51,8 +43,10 @@ class VisitsCountField_VisitsCountFieldType extends BaseFieldType
             'value' => $value
         );
 
-        return craft()->templates->render('visitscountfield/fields/VisitsCountField_VisitsCountFieldType.twig',
-            $variables);
+        return craft()->templates->render(
+            'visitscountfield/fields/VisitsCountField_VisitsCountFieldType.twig',
+            $variables
+        );
     }
 
     /**
--- models/VisitsCountField_VisitsCountModel.php
+++ models/VisitsCountField_VisitsCountModel.php
@@ -13,4 +13,12 @@ class VisitsCountField_VisitsCountModel extends BaseModel
             'visitsCount' => [AttributeType::Number, 'default' => 0],
         ]);
     }
+
+    /**
+     * @return string
+     */
+    function __toString()
+    {
+        return (string) $this->visitsCount;
+    }
 }
